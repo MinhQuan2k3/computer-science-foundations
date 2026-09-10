11d6ab1a8e6fbb6ca4a2ed17bbc732078e009bc9
MinhQuan2k3 <quannm18082018@gmail.com>
2026-09-10 12:31:20 +0700

    Update BankAccount.java

--- 02-oop/encapsulation/BankAccount.java
+++ 02-oop/encapsulation/BankAccount.java
@@ -1,1 +1,29 @@
+public class BankAccount {
 
+    private String ownerName;
+    private double balance;
+
+    public BankAccount(String ownerName, double initialBalance) {
+        this.ownerName = ownerName;
+        this.balance = initialBalance;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+        }
+    }
+
+    public boolean withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            return true;
+        }
+
+        return false;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
